8e3caef8e90dc55e508752b5728e6d450e1d12e3
lilmen <lilmen1313@gmail.com>
2022-05-29 13:42:58 +0300

    Создал функцию Drow
    Процедурный подход

--- OOP/OOP/Program.cs
+++ OOP/OOP/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int x1 = 1;
+int y1 = 3;
+char sym = '*';
+Draw(x1, y1, sym);
+
+void Draw (int x, int y, char sym)
+{
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine(sym);
+}
