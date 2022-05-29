7ebfc6d581f7b1cfab5e7ded86aea26b9067f7a6
lilmen <lilmen1313@gmail.com>
2022-05-29 19:09:54 +0300

    Добавил класс "точка" с методом внутри

--- OOP/OOP/Program.cs
+++ OOP/OOP/Program.cs
@@ -1,10 +1,10 @@
-﻿int x1 = 1;
-int y1 = 3;
-char sym = '*';
-Draw(x1, y1, sym);
-
-void Draw (int x, int y, char sym)
-{
-    Console.SetCursorPosition(x, y);
-    Console.WriteLine(sym);
-}
+﻿Point p1 = new Point();
+p1.x = 2;
+p1.y = 50;
+p1.sym = '#';
+p1.Draw();
+Point p2 = new Point();
+p2.x = 15;
+p2.y = 11;
+p2.sym = 'c';
+p2.Draw();
