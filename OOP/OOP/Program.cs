a62a1471917dc57fc5025d08a6687d8f802f7141
lilmen <lilmen1313@gmail.com>
2022-05-31 22:36:20 +0300

    Class List
    Добавил класс лист, содержащий тип данных - точка

--- OOP/OOP/Program.cs
+++ OOP/OOP/Program.cs
@@ -1,4 +1,26 @@
-﻿Point p1 = new Point(1, 4, '=');
+﻿
+Point p1 = new Point(1, 4, '=');
 p1.Draw();
-Point p2 = new Point(7, 4, '+');
+Point p2 = new Point(7, 8, '+');
 p2.Draw();
+Point p3 = new Point(7, 4, '-');
+p3.Draw();
+Point p4 = new Point(1, 6, '/');
+p4.Draw();
+
+List<Point> numList = new List<Point>();
+numList.Add(p1);
+numList.Add(p2);
+numList.Add(p3);
+numList.Add(p4);
+
+//int x = numList[0];
+//int y = numList[1];
+//int z = numList[2];
+
+numList.RemoveAt(0);
+
+foreach (Point i in numList)
+{ 
+    Console.WriteLine(i);
+}
